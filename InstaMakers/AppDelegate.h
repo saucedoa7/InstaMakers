e1b2548aabb5b605d8d8701e140a581eb20e6ee0
Albert Saucedo <saucedoa7@gmail.com>
2014-08-18 17:00:48 -0500

    First Commit B

---
+++ InstaMakers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaMakers
+//
+//  Created by Albert Saucedo on 8/18/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaMakers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InstaMakers
+//
+//  Created by Albert Saucedo on 8/18/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
